f426c24923d434e2b8356dbe128ddc13d42fcf06
toaderDragos <toader.dragosh@gmail.com>
2024-01-10 16:56:41 +0200

    1.Final app. All tests work as of january 2024.

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -2,13 +2,16 @@ package com.udacity.project4
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -23,6 +26,7 @@ import com.udacity.project4.util.DataBindingIdlingResource
 import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.endsWith
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -33,6 +37,7 @@ import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.get
+import org.koin.test.inject
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -43,8 +48,9 @@ class RemindersActivityTest :
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
     private val dataBindingIdlingResource = DataBindingIdlingResource()
-    private val reminder1 = ReminderDTO("TITLE1", "DESCRIPTION1", "LOCATION1", 23.44, 43.34)
+    private val reminder1 = ReminderDTO("TITLE1", "DESCRIPTION1", "LOCATION123", 23.44, 43.34)
     private val reminders = mutableListOf(reminder1)
+    private lateinit var saveViewModel: SaveReminderViewModel
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -75,6 +81,7 @@ class RemindersActivityTest :
         runTest {
             repository.deleteAllReminders()
         }
+
     }
 
     //      It ensures that Espresso waits for data binding to finish updating the UI before it tries to interact with it.
@@ -110,8 +117,15 @@ class RemindersActivityTest :
     )
 
     // test authenticationActivity
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun create_Reminder_Save_Reminder_Display_Reminder_Update_Reminder_Delete_Reminder() {
+    fun create_Reminder_Save_Reminder_Display_Reminder_Update_Reminder_Delete_Reminder() = runTest {
+
+        // Setting a fake location for the reminder including the latitude and longitude ( in SaveReminder Fragment they are needed for the geofence )
+        saveViewModel = inject<SaveReminderViewModel>().value
+        saveViewModel.reminderSelectedLocationStr.postValue(reminder1.location)
+        saveViewModel.latitude.postValue(reminder1.latitude)
+        saveViewModel.longitude.postValue(reminder1.longitude)
 
         // Launch RemindersActivity
         val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
@@ -128,59 +142,84 @@ class RemindersActivityTest :
         // Check if the save reminder screen is displayed - title, description, location, selectLocation
         // If the next lines are correct, then the save reminder screen is displayed implicitly
         onView(withId(R.id.selectLocation)).check(matches(isDisplayed()))
-        // Give a title and description to the reminder
+        // Give a title and description to the reminder // Update the reminder as we go along
         onView(withId(R.id.reminderTitle)).perform(replaceText("New Title"))
+        reminder1.title = "New Title"
         onView(withId(R.id.reminderDescription)).perform(replaceText("New Description"))
+        reminder1.description = "New Description"
 
         /*** Select location and map testing ***/
         // Click on the select location button
         onView(withId(R.id.selectLocation)).perform(click())
 
         // Wait for the map to zoom in to user's position
-        Thread.sleep(2000)
+        Thread.sleep(1500)
         // Check if the Select Location screen is displayed - map, saveLocation button, cancel
         onView(withId(R.id.map)).check(matches(isDisplayed()))
         // Check if the Save Location button is displayed
         onView(withId(R.id.saveLocationButton)).check(matches(isDisplayed()))
         // Click on the map  // May not select any location so this might go wrong
         onView(withId(R.id.map)).perform(click())
-        // Long click on the map
-        onView(withId(R.id.map)).perform(longClick())
 
-        /*** Test menu choices for map styling ***/
+        /*** Test menu choices for map styling. Thread.sleep is just for a nicer user experience ***/
         // Click on the menu
         openActionBarOverflowOrOptionsMenu(getApplicationContext())
         // Click on the Hybrid Map
         onView(withText("Hybrid Map")).perform(click())
+        Thread.sleep(500)
         // Click on the menu
         openActionBarOverflowOrOptionsMenu(getApplicationContext())
         // Click on the Satellite Map
         onView(withText("Satellite Map")).perform(click())
+        Thread.sleep(500)
         // Click on the menu
         openActionBarOverflowOrOptionsMenu(getApplicationContext())
         // Click on the Terrain Map
         onView(withText("Terrain Map")).perform(click())
+        Thread.sleep(500)
         // Click on the menu
         openActionBarOverflowOrOptionsMenu(getApplicationContext())
         // Click on the Normal Map
         onView(withText("Normal Map")).perform(click())
+        Thread.sleep(500)
 
-        // Click on the save location button
-        onView(withId(R.id.saveLocationButton)).perform(click())
+        // Just go back without saving because testing the selection of a POI is almost impossible
+        Espresso.pressBack()
 
         // In the save reminder screen check if the reminder title and description is still displayed
         onView(withId(R.id.reminderTitle)).check(matches(isDisplayed()))
         onView(withId(R.id.reminderDescription)).check(matches(isDisplayed()))
-        // Click on the save reminder button
-        onView(withId(R.id.saveReminder)).perform(click())
+        Thread.sleep(1000)
 
-        // Check if the reminder is displayed in the reminders list
-        onView(withText("New Title")).check(matches(isDisplayed()))
-        onView(withText("New Description")).check(matches(isDisplayed()))
+
+        // Check if the selected location is displayed
+        onView(withId(R.id.selectedLocation)).check(matches(withText(reminder1.location)))
+
+        // Click on the save reminder button   // Location should be already loaded because it was saved in the repository earlier
+        onView(withId(R.id.saveReminder)).perform(click())
+        Thread.sleep(500)
+
+        // Check if the reminder is displayed in the reminders list - they are hidden in a recyclerview so this is not the way
+//        onView(withText("New Title")).check(matches(isDisplayed()))
+//        onView(withText("New Description")).check(matches(isDisplayed()))
+
+        // monitor activityScenario for "idling" (used to flow control the espresso tests)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
 
         /*** Update the reminder testing ***/
-        // Click on the reminder
-        onView(withText("New Title")).perform(click())
+        // Click on the reminder - the reminder is in a recyclerview so it's not directly accessible
+        // onView(withText(endsWith("New Title"))).perform(click()) - wrong!
+
+        // verify that the ReminderListFragment is in view
+        onView(withId(R.id.reminderssRecyclerView)).check((matches(isDisplayed())))
+        // Works
+        onView(withId(R.id.reminderssRecyclerView))
+            .perform(
+                RecyclerViewActions.actionOnItem<ViewHolder>(
+                    hasDescendant(withText("New Title")),
+                    click()
+                )
+            )
 
         // Check if the clicked reminder details are displayed
         onView(withId(R.id.reminderTitle)).check(matches(isDisplayed()))
@@ -191,9 +230,11 @@ class RemindersActivityTest :
 
         // use idling resource to wait for data binding to finish
         dataBindingIdlingResource.monitorActivity(activityScenario)
-        Thread.sleep(2000)
+        Thread.sleep(1000)
 
-        // Check if the Save Location button is displayed
+        // Hide keyboard
+        onView(withId(R.id.reminderDescription)).perform(closeSoftKeyboard())
+        // Check if the Update Location button is displayed   - this is the id of the button in the UPDATE REMINDER screen
         onView(withId(R.id.updateReminder)).check(matches(isDisplayed()))
         // Click on the update reminder button
         onView(withId(R.id.updateReminder)).perform(click())
@@ -204,7 +245,11 @@ class RemindersActivityTest :
 
         /*** Delete the reminder testing ***/
         // Click on the reminder
-        onView(withText("Updated Title")).perform(click())
+        onView(withText(endsWith("Updated Title"))).perform(click())
+
+        // Hide keyboard
+        onView(withId(R.id.reminderDescription)).perform(closeSoftKeyboard())
+        Thread.sleep(1000)
         // Click on the delete reminder button
         onView(withId(R.id.deleteReminder)).perform(click())
         // Check if there are no reminders displayed
